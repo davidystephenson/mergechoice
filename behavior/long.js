b09be63ee8e21e087b2a0a289bcaffc43aef81e2
David Y. Stephenson <david@davidystephenson.com>
2025-04-17 21:32:09 -0400

    Test long flow

--- behavior/long.js
+++ behavior/long.js
@@ -36,6 +36,36 @@ index: 12
 catalog: eyes
 queue: time
 
+RANKING
+1. skyfall: 25
+2. quantum: 24
+3. casino2: 23
+4. die: 22
+5. world: 21
+6. tomorrow: 20
+7. goldeneye: 19
+8. license: 18
+9. daylights: 17
+10. view: 16
+11. never: 15
+12. octopussy: 14
+13. eyes: 13
+14. moonraker: 12
+15. loved: 11
+16. golden: 10
+17. live: 9
+18. diamonds: 8
+19. service: 7
+20. casino1: 6
+21. twice: 5
+22. thunderball: 4
+23. goldfinger: 3
+24. russia: 2
+24. cuaron: 2
+25. time: 1
+25. no: 1
+26. spectre: 0
+
 User chooses catalog
 */
 
@@ -76,6 +106,36 @@ CHOICE
 index: 6
 catalog: service
 queue: time
+
+RANKING
+1. skyfall: 26
+2. quantum: 25
+3. casino2: 24
+4. die: 23
+5. world: 22
+6. tomorrow: 21
+7. goldeneye: 20
+8. license: 19
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+14. moonraker: 12
+15. loved: 11
+16. golden: 10
+17. live: 9
+18. diamonds: 8
+19. service: 7
+20. casino1: 6
+21. twice: 5
+22. thunderball: 4
+23. goldfinger: 3
+24. russia: 2
+24. cuaron: 2
+25. no: 1
+25. time: 1
+26. spectre: 0
 
 User chooses queue
 */
@@ -123,6 +183,36 @@ index: 2
 catalog: golden
 queue: time
 
+RANKING
+1. skyfall: 26
+2. quantum: 25
+3. casino2: 24
+4. die: 23
+5. world: 22
+6. tomorrow: 21
+7. goldeneye: 20
+8. license: 19
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+14. moonraker: 12
+15. loved: 11
+16. golden: 10
+17. live: 9
+17. cuaron: 9
+18. diamonds: 8
+18. time: 8
+19. service: 7
+20. casino1: 6
+21. twice: 5
+22. thunderball: 4
+23. goldfinger: 3
+24. russia: 2
+25. no: 1
+26. spectre: 0
+
 User chooses queue
 */
 
@@ -168,6 +258,36 @@ CHOICE
 index: 1
 catalog: moonraker
 queue: time
+
+RANKING
+1. skyfall: 26
+2. quantum: 25
+3. casino2: 24
+4. die: 23
+5. world: 22
+6. tomorrow: 21
+7. goldeneye: 20
+8. license: 19
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+14. moonraker: 12
+14. cuaron: 12
+15. loved: 11
+15. time: 11
+16. golden: 10
+17. live: 9
+18. diamonds: 8
+19. service: 7
+20. casino1: 6
+21. twice: 5
+22. thunderball: 4
+23. goldfinger: 3
+24. russia: 2
+25. no: 1
+26. spectre: 0
 
 User chooses queue
 */
@@ -215,6 +335,36 @@ index: 6
 catalog: goldeneye
 queue: cuaron
 
+RANKING
+1. skyfall: 26
+2. quantum: 25
+3. casino2: 24
+4. die: 23
+5. world: 22
+6. tomorrow: 21
+7. goldeneye: 20
+8. license: 19
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+13. cuaron: 14
+14. time: 13
+15. moonraker: 12
+16. loved: 11
+17. golden: 10
+18. live: 9
+19. diamonds: 8
+20. service: 7
+21. casino1: 6
+22. twice: 5
+23. thunderball: 4
+24. goldfinger: 3
+25. russia: 2
+26. no: 1
+27. spectre: 0
+
 User chooses catalog
 */
 
@@ -261,7 +411,37 @@ index: 3
 catalog: view
 queue: cuaron
 
-User chooses queue
+RANKING
+1. skyfall: 27
+2. quantum: 26
+3. casino2: 25
+4. die: 24
+5. world: 23
+6. tomorrow: 22
+7. goldeneye: 21
+8. license: 19
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+13. cuaron: 14
+14. time: 13
+15. moonraker: 12
+16. loved: 11
+17. golden: 10
+18. live: 9
+19. diamonds: 8
+20. service: 7
+21. casino1: 6
+22. twice: 5
+23. thunderball: 4
+24. goldfinger: 3
+25. russia: 2
+26. no: 1
+27. spectre: 0
+
+User chooses catalog
 */
 
 export const operation7 = {
@@ -307,6 +487,36 @@ index: 1
 catalog: license
 queue: cuaron
 
+RANKING
+1. skyfall: 27
+2. quantum: 26
+3. casino2: 25
+4. die: 24
+5. world: 23
+6. tomorrow: 22
+7. goldeneye: 21
+8. license: 19
+9. cuaron: 18
+9. daylights: 18
+10. view: 17
+11. never: 16
+12. octopussy: 15
+13. eyes: 14
+14. time: 13
+15. moonraker: 12
+16. loved: 11
+17. golden: 10
+18. live: 9
+19. diamonds: 8
+20. service: 7
+21. casino1: 6
+22. twice: 5
+23. thunderball: 4
+24. goldfinger: 3
+25. russia: 2
+26. no: 1
+27. spectre: 0
+
 User chooses queue
 */
 
@@ -346,5 +556,40 @@ export const operation8 = {
   queue: []
 }
 /*
+CHOICE
+index: 0
+catalog: []
+queue: []
+
+RANKING
+1. skyfall: 27
+2. quantum: 26
+3. casino2: 25
+4. die: 24
+5. world: 23
+6. tomorrow: 22
+7. goldeneye: 21
+8. cuaron: 20
+9. license: 19
+10. daylights: 18
+11. view: 17
+12. never: 16
+13. octopussy: 15
+14. eyes: 14
+15. time: 13
+16. moonraker: 12
+17. loved: 11
+18. golden: 10
+19. live: 9
+20. diamonds: 8
+21. service: 7
+22. casino1: 6
+23. twice: 5
+24. thunderball: 4
+25. goldfinger: 3
+26. russia: 2
+27. no: 1
+28. spectre: 0
+
 complete
 */
